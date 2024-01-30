131f7eb55f94463fcda45ef3dab48ed0cbf0b13f
Lior Lavi <41231135+liorlavi1@users.noreply.github.com>
2024-01-30 18:50:04 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install ruby -y
-sudo yum install wget -y
 
 # Install Docker
 sudo yum install docker -y
@@ -11,9 +9,3 @@ sudo usermod -a -G docker ec2-user
 # Install Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-# Install code deploy agent
-wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-systemctl start codedeploy-agent
